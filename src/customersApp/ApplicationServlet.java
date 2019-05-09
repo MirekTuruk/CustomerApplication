9825f575264bd60733fba981875ef2bd8b984c75
MirekTuruk <miroslaw.turuk@gmail.com>
2019-05-10 00:23:50 +0200

    second

--- src/customersApp/ApplicationServlet.java
+++ src/customersApp/ApplicationServlet.java
@@ -2,6 +2,12 @@ package customersApp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +18,20 @@ public class ApplicationServlet extends HttpServlet {
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 	res.setContentType("text/plain; charset=utf-8");
-	PrintWriter printWriter = res.getWriter();
-	printWriter.println("LISTA");
+	try {
+		Driver driver = new com.mysql.jdbc.Driver();
+		DriverManager.registerDriver(driver);
+		Connection connection = driver.connect("jdbc:mysql://localhost/baza?user=root:password=password", null);
+		Statement statement = connection.createStatement();
+		if(statement.execute("SELECT * FROM customerList")) {
+			ResultSet list = statement.getResultSet();
+			while (list.next());
+			res.getWriter().printf(list.getString("name"), list.getString("surname"), list.getString("phoneNr"), list.getString("mailAddres"));
+		}
+		connection.close();
+	}catch (SQLException e) {
+		e.printStackTrace();
+	}
 }
 }
 
